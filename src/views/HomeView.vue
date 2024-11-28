4d97277b59f1779a4d506a2b6fe090ba695703ef
Josh Early <joshe@Joshs-MBP.home>
2024-11-28 09:21:03 +0200

    job listing project to navbar active link

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue';
+import JobListings from '@/components/JobListings.vue';
+</script>
+
+<template>
+  <Hero
+    title="Become a Vue Dev"
+    subtitle="Find a Vue job that fits your skills"
+  />
+  <HomeCards />
+  <JobListings :limit="3" :show-button="true" />
+</template>
+
+<style></style>
